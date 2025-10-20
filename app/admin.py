71695a116f2e82842f262059769283fadf7147cc
Flavia Ramos <fla.bar.ram@gmail.com>
2025-10-20 14:33:46 -0300

    Delete app directory

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Aluno
-
-@admin.register(Aluno)
-class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'matricula', 'criado_em')
-from django.contrib import admin
-
-# Register your models here.
